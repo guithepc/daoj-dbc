646ae83e0efa9363525a05561a44c70fe01ed668
Guilherme <guilherme.pc@yahoo.com>
2022-05-10 06:05:01 -0300

    Seller.class implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller sll = new  Seller(1, "Gui", "agmail.com", new Date(), 20000.00, obj);
+		System.out.println(sll);
 	}
 }
