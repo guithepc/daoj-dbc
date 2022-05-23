6d53b71d6e77aebaca46ab4a0a904e0f9ad8612f
Guilherme <guilherme.pc@yahoo.com>
2022-05-23 06:24:49 -0300

    reutilizando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sl = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1 - FindById ===");
 		Seller seller = sl.findById(2);
 		
 		System.out.println(seller);
--- src/model/dao/impl/SellerDaoJdbcImpl.java
+++ src/model/dao/impl/SellerDaoJdbcImpl.java
@@ -55,17 +55,8 @@ public class SellerDaoJdbcImpl implements SellerDao{
 			rs = st.executeQuery();
 			
 			if (rs.next()) {
-				Department department = new Department();
-				department.setId(rs.getInt("DepartmentId"));
-				department.setName(rs.getString("DepName"));
-				
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBithDate(rs.getDate("BirthDate"));
-				seller.setDepartment(department);
+				Department department = instantiateDepartment(rs);
+				Seller seller = instantiateSeller(rs, department);
 				
 				return seller;
 			}
@@ -77,6 +68,26 @@ public class SellerDaoJdbcImpl implements SellerDao{
 			DB.closeStatement(st);
 		}
 		
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department department) throws SQLException {
+		
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBithDate(rs.getDate("BirthDate"));
+		seller.setDepartment(department);
+		
+		return seller;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department department = new Department();
+		department.setId(rs.getInt("DepartmentId"));
+		department.setName(rs.getString("DepName"));
+		return department;
 	}
 
 	@Override
